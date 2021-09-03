1f6b9781665a45ab0395940b9619d5da58e7ba7a
Lori Culberson <bigskybandit@sbcglobal.net>
2021-09-02 23:20:51 -0400

    Add cors

--- server/routes/api/todoRoutes.js
+++ server/routes/api/todoRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
-const db = require('../../models')
+const Todo = require('../../models/Todo')
 
 //localhost:3001/api/todos/
 router.get('/', async (req, res) => {
   const data = await Todo.find({})
-
+  console.log('data!!!', data)
   res.status(200).json(data)
 })
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const path = require('path');
-const db = require('./config/connection');
+const db = require('./config/connection')
+const routes = require('./routes')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +13,10 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes)
+app.use(cors({
+  origin: 'http://localhost:3000'
+}))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
